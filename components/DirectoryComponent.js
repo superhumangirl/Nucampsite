6128d76b515980b1d8ca82d158f70d75c28304fa
superhumangirl <83243361+superhumangirl@users.noreply.github.com>
2021-08-03 17:32:37 -0600

    All changes through animations

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { Component } from 'react';
+import { View, FlatList } from 'react-native';
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
-function Directory(props) {
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites
+    };
+};
 
-    const renderDirectoryItem = ({item}) => {
-        return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-            />
-        );
+class Directory extends Component {
+
+    static navigationOptions = {
+        title: 'Directory'
     };
     
-    return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    )
+    render() {
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        imageSrc={{uri: baseUrl + item.image}}
+                    />
+                </Animatable.View>
+            );
+        };
+        
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{props.campsites.errMess}</Text>
+                </View>
+            );
+        }
+        return (
+            <FlatList
+                data={this.props.campsites.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        );
+    }
 }
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
