df779281df24c2587e56e915d571f3fe87a250ab
superhumangirl <83243361+superhumangirl@users.noreply.github.com>
2021-08-07 12:19:48 -0600

    After Week 3 Workshop. Update ReservationComponet modal with alert. Add gesture to CampsiteInfoComponent.

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -12,7 +12,11 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Animatable.View 
+                animation='fadeInDown' 
+                duration={2000} 
+                delay={1000}
+            >
                     <Card title="Contact Information" wrapperStyle={{margin: 20}}>
                         <Text>
                             1 Nucamp Way
